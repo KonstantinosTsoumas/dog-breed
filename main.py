6218fb1e765b8e1bc6ca5e6c2d77e7afe1f7a02c
Konstantinos <tsoumascon@gmail.com>
2024-01-02 19:39:09 +0100

    add main

---
+++ main.py
@@ -0,0 +1,58 @@
+from src import logger
+from src.pipeline.data_ingestion_01 import DataIngestionTrainingPipeline
+from src.pipeline.prepare_base_model_02 import PrepareBaseModelTrainingPipeline
+# from src.pipeline.stage_03_training import ModelTrainingPipeline
+# from src.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion phase"
+try:
+   logger.info(f" {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f" {STAGE_NAME} has been completed. \n End of Ingestion phase.")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+# STAGE_NAME = "Prepare base model"
+# try:
+#    logger.info(f" {STAGE_NAME} started <<<<<<")
+#    prepare_base_model = PrepareBaseModelTrainingPipeline()
+#    prepare_base_model.main()
+#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+
+# STAGE_NAME = "Training"
+# try:
+#    logger.info(f" {STAGE_NAME} started <<<<<<")
+#    model_trainer = ModelTrainingPipeline()
+#    model_trainer.main()
+#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+#
+#
+#
+#
+#
+#
+# STAGE_NAME = "Evaluation phase"
+# try:
+#    logger.info(f" {STAGE_NAME} started <<<<<<")
+#    model_evalution = EvaluationPipeline()
+#    model_evalution.main()
+#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
+#
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
