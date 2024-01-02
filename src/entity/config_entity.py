ad0289c17988d4d474c39ab35f8793caa67e7011
Konstantinos <tsoumascon@gmail.com>
2024-01-02 20:22:59 +0100

    add misc

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -34,3 +34,37 @@ class PrepareBaseModelConfig:
         params_classes: int
         params_freeze_all: bool = field(default=True)
         params_freeze_till: int = field(default=2)
+
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    """
+    Configuration for callbacks during model training, including tensorboard and checkpoint paths.
+    """
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    """
+    Configuration for model training, including paths, training data, and training parameters.
+    """
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    """
+    Configuration for model evaluation, including model path, data, and evaluation parameters.
+    """
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
