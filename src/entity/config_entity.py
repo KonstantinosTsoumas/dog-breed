b7222df3dd514022dde444d08bef7cc013c54edf
Konstantinos <tsoumascon@gmail.com>
2024-01-02 20:38:00 +0100

    import field library

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,13 +1,5 @@
-from src.constants import *
-import os
 from pathlib import Path
-from dataclasses import dataclass
-from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,
-                                                PrepareCallbacksConfig,
-                                                TrainingConfig,
-                                                EvaluationConfig)
+from dataclasses import dataclass, field
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -24,16 +16,16 @@ class PrepareBaseModelConfig:
     """
     Configuration for preparing the base model, including paths and model parameters.
     """
-        root_dir: Path
-        base_model_path: Path
-        updated_base_model_path: Path
-        params_image_size: list
-        params_learning_rate: float
-        params_include_top: bool
-        params_weights: str
-        params_classes: int
-        params_freeze_all: bool = field(default=True)
-        params_freeze_till: int = field(default=2)
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+    params_freeze_all: bool = field(default=True)
+    params_freeze_till: int = field(default=2)
 
 @dataclass(frozen=True)
 class PrepareCallbacksConfig:
