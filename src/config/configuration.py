783a1608b86ade77ec0398d99985cb2c5c4e3e7e
Konstantinos <tsoumascon@gmail.com>
2024-01-02 21:08:08 +0100

    add local source path to ingestion pipeline

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -43,7 +43,8 @@ class ConfigurationManager:
 
         data_ingestion_config = DataIngestionConfig(
             root_dir=config['root_dir'],
-            source_URL=config['source_URL'],
+            source_url=config['source_url'],
+            local_source_file=config.get('local_source_file'),
             local_data_file=config['local_data_file'],
             unzip_dir=config['unzip_dir']
         )
