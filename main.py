56b1b743156c532c8636b8b645618104878d9ed8
Konstantinos <tsoumascon@gmail.com>
2024-01-04 21:42:08 +0100

    add base model preparation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src import logger
 from src.pipeline.data_ingestion_01 import DataIngestionTrainingPipeline
 from src.pipeline.prepare_base_model_02 import PrepareBaseModelTrainingPipeline
-# from src.pipeline.stage_03_training import ModelTrainingPipeline
+# from src.pipeline.training_model_03 import ModelTrainingPipeline
 # from src.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
@@ -10,49 +10,18 @@ try:
    logger.info(f" {STAGE_NAME} started <<<<<<")
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
-   logger.info(f" {STAGE_NAME} has been completed. \n End of Ingestion phase.")
+   logger.info(f" {STAGE_NAME} has been completed.")
 except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Prepare base model"
+try:
+   logger.info(f" {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f" {STAGE_NAME} has been completed.")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
-
-
-# STAGE_NAME = "Prepare base model"
-# try:
-#    logger.info(f" {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-
-
-
-
-# STAGE_NAME = "Training"
-# try:
-#    logger.info(f" {STAGE_NAME} started <<<<<<")
-#    model_trainer = ModelTrainingPipeline()
-#    model_trainer.main()
-#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-#
-#
-#
-#
-#
-#
-# STAGE_NAME = "Evaluation phase"
-# try:
-#    logger.info(f" {STAGE_NAME} started <<<<<<")
-#    model_evalution = EvaluationPipeline()
-#    model_evalution.main()
-#    logger.info(f" {STAGE_NAME} has been completed <<<<<<\n\nx==========x")
-#
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
