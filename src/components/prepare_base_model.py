63a52265f30ed6fb415ece8e1d8806ed09ed2224
Konstantinos <tsoumascon@gmail.com>
2024-01-14 21:40:24 +0100

    add: implementation of PrepareBaseModel class for configuring and updating ResNet101V2 base model

--- src/components/prepare_base_model.py
+++ src/components/prepare_base_model.py
@@ -1,69 +1,64 @@
 import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
 from pathlib import Path
-import torch
-import torchvision.models as models
-from torch import nn, optim
 from src.entity.config_entity import PrepareBaseModelConfig
 
 
-class PrepareBaseModel(nn.Module):
-    def __init__(self, config):
-        super(PrepareBaseModel, self).__init__()
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
-        self.params_freeze_all = self.config.params_freeze_all
-        self.params_freeze_till = self.config.params_freeze_till  # Corrected this line
-        self.model = self.get_base_model()
-        self.full_model, self.optimizer, self.loss_fn = self.update_base_model()
 
     def get_base_model(self):
-        # Load the pre-trained ResNet101 model
-        model = models.resnet101(pretrained=self.config.params_weights)
-
-        # If not including the fully connected top layer
-        if not self.config.params_include_top:
-            model = nn.Sequential(*list(model.children())[:-2])
-        return model
-
-    def _prepare_full_model(self, model, classes, freeze_all, freeze_till, learning_rate):
-        if freeze_all:
-            for param in model.parameters():
-                param.requires_grad = False
-        elif freeze_till is not None and freeze_till > 0:
-            ct = 0
-            for child in model.children():
-                ct += 1
-                if ct < freeze_till:
-                    for param in child.parameters():
-                        param.requires_grad = False
-
-        if isinstance(model, nn.Sequential):
-            in_features = 2048  # Beware reader: This is specific to ResNet101
-            model = nn.Sequential(
-                model,
-                nn.Flatten(),
-                nn.Linear(in_features, classes),
-                nn.Softmax(dim=1)
-            )
-        else:
-            # Replace the last fully connected layer
-            num_features = model.fc.in_features
-            model.fc = nn.Linear(num_features, classes)
-            model.add_module('softmax', nn.Softmax(dim=1))
-
-        optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-        loss_fn = nn.CrossEntropyLoss()
-        return model, optimizer, loss_fn
-
-    def update_base_model(self):
-        full_model, optimizer, loss_fn = self._prepare_full_model(
-            model=self.model,
-            classes=self.config.params_classes,
-            freeze_all=self.config.params_freeze_all,
-            freeze_till=self.config.params_freeze_till,
-            learning_rate=self.config.params_learning_rate
+        self.model = tf.keras.applications.ResNet101V2(
+            input_shape=self.config.params_image_size,
+            weights=self.config.params_weights,
+            include_top=self.config.params_include_top
         )
-        return full_model, optimizer, loss_fn
+
+        self.save_model(path=self.config.base_model_path, model=self.model)
 
     @staticmethod
-    def save_model(path, model):
-        torch.save(model.state_dict(), path)
+    def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                model.trainable = False
+
+        flatten_in = tf.keras.layers.Flatten()(model.output)
+        prediction = tf.keras.layers.Dense(
+            units=classes,
+            activation="softmax"
+        )(flatten_in)
+
+        full_model = tf.keras.models.Model(
+            inputs=model.input,
+            outputs=prediction
+        )
+
+        full_model.compile(
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
