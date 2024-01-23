0c9b979a23c913337c6a49cf715683ac333fc2d7
Konstantinos <tsoumascon@gmail.com>
2024-01-23 21:34:21 +0100

    fix: libraries identation

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -3,10 +3,10 @@ import os
 from pathlib import Path
 from src.utils.auxiliary_functions import read_yaml, create_directories
 from src.entity.config_entity import (DataIngestionConfig,
-                                                PrepareBaseModelConfig,
-                                                PrepareCallbacksConfig,
-                                                TrainingConfig,
-                                                EvaluationConfig)
+                                      PrepareBaseModelConfig,
+                                      PrepareCallbacksConfig,
+                                      TrainingConfig,
+                                      EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -140,6 +140,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model=Path("artifacts/training/model.h5"),
             training_data=Path("artifacts/data_ingestion"),
+            figures=Path("figures/evaluation"),
             artifacts=Path("artifacts"),
             all_params=self.params,
             params_image_size=self.params['IMAGE_SIZE'],
