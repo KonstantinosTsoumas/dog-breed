aa62e95ff8fb4f26c6410cc28ef8cdc465cc0b5e
Konstantinos <tsoumascon@gmail.com>
2024-01-23 21:32:51 +0100

    add: libraries, precision-recall curve, classification report

--- src/components/evaluation.py
+++ src/components/evaluation.py
@@ -6,11 +6,15 @@ from sklearn.metrics import (
     precision_recall_curve, average_precision_score
 )
 from src.entity.config_entity import EvaluationConfig
+from src.utils.auxiliary_functions import save_json
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 from pathlib import Path
-from src.utils.auxiliary_functions import save_json
 from sklearn.preprocessing import LabelEncoder, label_binarize
+from datetime import datetime
+import os
+from sklearn.metrics import classification_report
+
 
 class Evaluation:
 
@@ -110,8 +114,37 @@ class Evaluation:
         scores['roc_auc_scores'] = roc_auc_scores
         scores['average_precision_scores'] = average_precisions
 
-        return scores
-        #self.save_score(scores)
+        # Creating a classification report per class
+        report = classification_report(y_true, y_pred)
+
+        # Writing report to tzt
+        with open('classification_report.txt', 'w') as file:
+            file.write(report)
+
+        # Get precision, recall for each class
+        precision = dict()
+        recall = dict()
+        average_precision = dict()
+        for i in range(self.num_classes):
+            precision[i], recall[i], _ = precision_recall_curve(y_true_binarized[:, i], predictions[:, i])
+            average_precision[i] = average_precision_score(y_true_binarized[:, i], predictions[:, i])
+
+        # Plot precision-recall curve for each class
+        for i in range(self.num_classes):
+            fig, ax = plt.subplots()
+            # Plot the precision-recall curve
+            ax.step(recall[i], precision[i], where='post')
+            # Set labels, limits and title
+            ax.set_xlabel('Recall')
+            ax.set_ylabel('Precision')
+            ax.set_ylim([0.0, 1.05])
+            ax.set_xlim([0.0, 1.0])
+            ax.set_title(f'Precision-Recall curve: AP={average_precision[i]:0.2f}')
+            self.save_plot(fig=fig, path=self.config.figures, filename=f"classification_report_class_{i}.png")
+            # Free up memory
+            plt.close(fig)
+
+        return scores, y_true_binarized, predictions
 
     def save_score(self, scores):
         """
@@ -124,3 +157,51 @@ class Evaluation:
 
         save_json(path=Path("scores.json"), data=scores)
 
+    def plot_top_roc_curves(self, scores, y_true_binarized, predictions, top_n=20):
+        """
+        This method plots the top 20 classes (ROC).
+
+        Args:
+            scores: dict, A dictionary containing various evaluation metrics.
+            y_true_binarized: array (like), The true binary labels in binary label indicators.
+            predictions: array-like, Predictions of the model.
+            top_n: int, The top N classes to plot (default = 20).
+        returns: -
+        """
+
+        avg_precision_scores = scores['average_precision_scores']
+        sorted_classes = sorted(avg_precision_scores, key=avg_precision_scores.get, reverse=True)[:top_n]
+
+        # Define the figure and axis object
+        fig, ax = plt.subplots(figsize=(15, 10))
+
+        for class_label in sorted_classes:
+            class_index = int(class_label.split('_')[-1])
+            y_true_binary = y_true_binarized[:, class_index]
+            y_score = predictions[:, class_index]
+
+            fpr, tpr, _ = roc_curve(y_true_binary, y_score)
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, lw=2, label=f'Class {class_index} (AUC = {roc_auc:.2f})')
+
+        ax.set_xlabel('False Positive Rate')
+        ax.set_ylabel('True Positive Rate')
+        ax.set_title(f'Top {top_n} ROC Curves')
+        ax.legend(loc="lower right")
+
+        current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        filename = f"roc_curve_{current_time}.png"
+        self.save_plot(fig=fig, path=self.config.figures, filename=filename)
+    @staticmethod
+    def save_plot(fig, path: str, filename: str):
+        """
+        This function saves a plot to the specified path.
+
+        Args:
+            fig : matplotlib.figure.Figure, the figure object to be saved.
+            path : str, the directory path where the plot should be saved.
+            filename : str, the filename for the saved plot.
+        returns: -
+        """
+        full_path = os.path.join(path, filename)
+        fig.savefig(full_path)
