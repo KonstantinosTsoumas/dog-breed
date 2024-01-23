5e24e47a4e6efc49890188654e7c2f13de4364a7
Konstantinos <tsoumascon@gmail.com>
2024-01-23 21:36:47 +0100

    add: every step of the process

--- main.py
+++ main.py
@@ -8,41 +8,41 @@ from src.pipeline.evaluation_04 import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion phase"
 try:
-   logger.info(f" {STAGE_NAME} started !")
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f" {STAGE_NAME} has been completed.")
+    logger.info(f" {STAGE_NAME} started !")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f" {STAGE_NAME} has been completed.")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Prepare base model"
 try:
-   logger.info(f" {STAGE_NAME} started !")
-   prepare_base_model = PrepareBaseModelTrainingPipeline()
-   prepare_base_model.main()
-   logger.info(f" {STAGE_NAME} has been completed.")
+    logger.info(f" {STAGE_NAME} started !")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f" {STAGE_NAME} has been completed.")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Training phase"
 try:
-   logger.info(f" {STAGE_NAME} started !")
-   model_trainer = ModelTrainingPipeline()
-   model_trainer.main()
-   logger.info(f" {STAGE_NAME} has been completed.")
+    logger.info(f" {STAGE_NAME} started !")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f" {STAGE_NAME} has been completed.")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Evaluation phase"
 try:
-   logger.info(f" {STAGE_NAME} started")
-   model_evalution = EvaluationPipeline()
-   model_evalution.main()
-   logger.info(f" {STAGE_NAME} has been completed.")
+    logger.info(f" {STAGE_NAME} started")
+    model_evaluation = EvaluationPipeline()
+    model_evaluation.main()
+    logger.info(f" {STAGE_NAME} has been completed.")
 
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
